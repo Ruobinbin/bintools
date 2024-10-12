9c655852d842fcc60a8a810d788f7f809b652741
Ruobin <1773274301@qq.com>
2024-10-12 21:02:26 +0800

    统一到main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,25 @@
 import { createApp } from "vue";
-import App from "./App.vue";
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
 
-createApp(App).mount("#app");
+import App from "./App.vue";
+import Novel from "./views/novel/novel.vue";
+import Setting from "./views/setting/setting.vue";
+
+const app = createApp(App);
+app.use(ElementPlus);
+if (document.getElementById("app")) {
+    app.mount("#app");
+}
+
+const novel = createApp(Novel);
+novel.use(ElementPlus);
+if (document.getElementById("novel")) {
+    novel.mount("#novel");
+}
+
+const setting = createApp(Setting);
+setting.use(ElementPlus);
+if (document.getElementById("setting")) {
+    setting.mount("#setting");
+}
