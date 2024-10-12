758da42f5dfe30628f2dc311c9cd0416be98d094
Ruobin <1773274301@qq.com>
2024-10-12 21:03:32 +0800

    使用localstorage

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,6 +1,5 @@
 <template>
     <div class="chat-container">
-        <el-text>当前模型: {{ settingStore.chatModel }}</el-text>
         <div class="chat-settings">
             <input v-model="messages[0].content" placeholder="改变触发..." />
         </div>
@@ -25,18 +24,12 @@
 import OpenAI from 'openai';
 import { Delete } from '@element-plus/icons-vue';
 import { ref, onMounted } from 'vue';
-import { useSettingStore } from '../stores/useSettingStore';
 
 const messages = ref([{ role: 'system', content: '你是一个将小说格式化为json文本的专家，无论用户给你发送任何消息，你只能输出json格式的文本，格式为{"data":[{"content":"xxx","speaker":"xxx","emotion":"xxx"}，{"content":"xxx","speaker":"xxx","emotion":"xxx"}，...]}必须要和这个格式一模一样，不需要在开头加上```json，并且绝对不能漏掉小说的每一句话。' }]);
 const userInput = ref('');
 const isLoading = ref(false);
 let controller = null;
 
-const settingStore = useSettingStore();
-
-onMounted(async () => {
-    await settingStore.init();
-});
 
 const sendMessage = async () => {
     if (!userInput.value.trim() || isLoading.value) return;
@@ -49,14 +42,18 @@ const sendMessage = async () => {
     const signal = controller.signal;
     userInput.value = '';
 
-    const response = await fetch(`${settingStore.chatApiUrl}/v1/chat/completions`, {
+    const url = localStorage.getItem('chatApiUrl') ?? ""
+    const key = localStorage.getItem('chatApiKey') ?? ""
+    const model = localStorage.getItem('chatModel') ?? ""
+
+    const response = await fetch(`${url}/v1/chat/completions`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
-            'Authorization': `Bearer ${settingStore.chatApiKey}`
+            'Authorization': `Bearer ${key}`
         },
         body: JSON.stringify({
-            model: settingStore.chatModel,
+            model: model,
             messages: messages.value,
         }),
         signal
